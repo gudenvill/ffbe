c7898ca8989f986edd1df5d93d89a5b4f84f8086
gudenvill <amirtap@ymail.com>
2025-12-14 18:05:58 +0100

    add EJS template engine

--- app.js
+++ app.js
@@ -4,6 +4,10 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+// View engine
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
 // Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
